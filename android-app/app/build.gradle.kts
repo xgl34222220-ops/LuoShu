02c7b0c86ecb3474fbe6855fd7c7851b15de6a84
xgl34222220-ops <xgl34222220@gmail.com>
2026-07-18 12:03:57 +0800

    app: remove Compose tooling from installable test APK

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -45,7 +45,4 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-
-    debugImplementation("androidx.compose.ui:ui-tooling")
 }
