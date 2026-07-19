d62e937b390a83e47971d9ac353f3e177e1e55be
xgl34222220-ops <xgl34222220@gmail.com>
2026-07-19 14:14:33 +0800

    feat(ui): provide appearance settings through CompositionLocal

---
+++ android-app/app/src/main/java/io/github/xgl34222220/luoshu/ui/appearance/LocalAppearance.kt
@@ -0,0 +1,5 @@
+package io.github.xgl34222220.luoshu.ui.appearance
+
+import androidx.compose.runtime.staticCompositionLocalOf
+
+val LocalAppearanceSettings = staticCompositionLocalOf { AppearanceSettings() }
