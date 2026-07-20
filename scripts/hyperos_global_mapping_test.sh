1ed5fde0bce27ca6df7390a47ebe64aac0e91aef
xgl34222220-ops <xgl34222220@gmail.com>
2026-07-20 18:44:12 +0800

    test(hyperos): verify XML-driven device font targets

--- scripts/hyperos_global_mapping_test.sh
+++ scripts/hyperos_global_mapping_test.sh
@@ -10,10 +10,15 @@ USER_FONTS_DIR="$ROOT/user-fonts"
 LUOSHU_SYSTEM_FONTS_ROOT="$ROOT/real/system/fonts"
 LUOSHU_PRODUCT_FONTS_ROOT="$ROOT/real/product/fonts"
 LUOSHU_SYSTEM_EXT_FONTS_ROOT="$ROOT/real/system_ext/fonts"
-export MODULE_DIR MODDIR USER_FONTS_DIR LUOSHU_SYSTEM_FONTS_ROOT LUOSHU_PRODUCT_FONTS_ROOT LUOSHU_SYSTEM_EXT_FONTS_ROOT
+LUOSHU_FONT_XMLS="$ROOT/fonts.xml"
+LUOSHU_PYTHON=$(command -v python3)
+export MODULE_DIR MODDIR USER_FONTS_DIR LUOSHU_SYSTEM_FONTS_ROOT LUOSHU_PRODUCT_FONTS_ROOT LUOSHU_SYSTEM_EXT_FONTS_ROOT LUOSHU_FONT_XMLS LUOSHU_PYTHON
 
 mkdir -p "$MODULE_DIR/system/fonts" "$MODULE_DIR/product/fonts" "$MODULE_DIR/system_ext/fonts" \
+    "$MODULE_DIR/common" "$MODULE_DIR/config" \
     "$USER_FONTS_DIR" "$LUOSHU_SYSTEM_FONTS_ROOT" "$LUOSHU_PRODUCT_FONTS_ROOT" "$LUOSHU_SYSTEM_EXT_FONTS_ROOT"
+cp "$REPO_ROOT/common/font_config_targets.py" "$MODULE_DIR/common/font_config_targets.py"
+
 printf 'regular-source\n' > "$USER_FONTS_DIR/Demo-Regular.ttf"
 printf 'medium-source\n' > "$USER_FONTS_DIR/Demo-Medium.ttf"
 printf 'bold-source\n' > "$USER_FONTS_DIR/Demo-Bold.ttf"
@@ -24,7 +29,27 @@ printf 'stock-700\n' > "$LUOSHU_PRODUCT_FONTS_ROOT/700.ttf"
 printf 'stock-roboto\n' > "$LUOSHU_SYSTEM_FONTS_ROOT/Roboto-Regular.ttf"
 printf 'stock-medium\n' > "$LUOSHU_PRODUCT_FONTS_ROOT/Roboto-Medium.ttf"
 printf 'stock-google-bold\n' > "$LUOSHU_SYSTEM_EXT_FONTS_ROOT/GoogleSans-Bold.ttf"
+printf 'stock-dynamic-regular\n' > "$LUOSHU_SYSTEM_FONTS_ROOT/ActualSansVF.ttf"
+printf 'stock-dynamic-medium\n' > "$LUOSHU_PRODUCT_FONTS_ROOT/ActualSans-Medium.ttf"
+printf 'stock-source-bold\n' > "$LUOSHU_SYSTEM_EXT_FONTS_ROOT/SourceSansPro-Bold.ttf"
+printf 'stock-serif\n' > "$LUOSHU_SYSTEM_FONTS_ROOT/NotoSerif-Regular.ttf"
 printf 'stale-overlay\n' > "$MODULE_DIR/system/fonts/Roboto-Regular.ttf"
+
+cat > "$LUOSHU_FONT_XMLS" <<'EOF_XML'
+<?xml version="1.0" encoding="utf-8"?>
+<familyset>
+  <family name="sans-serif">
+    <font weight="400">ActualSansVF.ttf<axis tag="wght" stylevalue="400"/></font>
+    <font weight="500">ActualSans-Medium.ttf</font>
+  </family>
+  <family name="source-sans-pro">
+    <font weight="700">SourceSansPro-Bold.ttf</font>
+  </family>
+  <family name="serif">
+    <font weight="400">NotoSerif-Regular.ttf</font>
+  </family>
+</familyset>
+EOF_XML
 
 _font_store_reset() {
     rm -rf "$1/.luoshu-font-store"
@@ -65,6 +90,13 @@ test "$(cat "$MODULE_DIR/product/fonts/700.ttf")" = 'bold-source'
 test "$(cat "$MODULE_DIR/system/fonts/Roboto-Regular.ttf")" = 'regular-source'
 test "$(cat "$MODULE_DIR/product/fonts/Roboto-Medium.ttf")" = 'medium-source'
 test "$(cat "$MODULE_DIR/system_ext/fonts/GoogleSans-Bold.ttf")" = 'bold-source'
+test "$(cat "$MODULE_DIR/system/fonts/ActualSansVF.ttf")" = 'regular-source'
+test "$(cat "$MODULE_DIR/product/fonts/ActualSans-Medium.ttf")" = 'medium-source'
+test "$(cat "$MODULE_DIR/system_ext/fonts/SourceSansPro-Bold.ttf")" = 'bold-source'
+test ! -e "$MODULE_DIR/system/fonts/NotoSerif-Regular.ttf"
+grep -qx 'ActualSansVF.ttf' "$MODULE_DIR/config/hyperos_dynamic_targets.conf"
+grep -qx 'ActualSans-Medium.ttf' "$MODULE_DIR/config/hyperos_dynamic_targets.conf"
+grep -qx 'SourceSansPro-Bold.ttf' "$MODULE_DIR/config/hyperos_dynamic_targets.conf"
 test ! -e "$MODULE_DIR/system/fonts/MiSansVF.ttf"
 test ! -e "$MODULE_DIR/product/fonts/Roboto-Regular.ttf"
 test ! -e "$MODULE_DIR/system_ext/fonts/Roboto-Regular.ttf"
