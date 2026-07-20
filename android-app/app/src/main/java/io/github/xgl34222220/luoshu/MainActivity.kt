330af8ea7a3fcfb135d02cccfb91ea525e2b6352
xgl34222220-ops <xgl34222220@gmail.com>
2026-07-20 09:22:01 +0800

    feat(import): open task center from notification intent

--- android-app/app/src/main/java/io/github/xgl34222220/luoshu/MainActivity.kt
+++ android-app/app/src/main/java/io/github/xgl34222220/luoshu/MainActivity.kt
@@ -1,16 +1,29 @@
 package io.github.xgl34222220.luoshu
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
+    private var openTaskCenter by mutableStateOf(false)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        openTaskCenter = intent.getBooleanExtra(EXTRA_OPEN_TASK_CENTER, false)
         setContent {
-            LuoShuHost()
+            if (openTaskCenter) TaskCenterHost() else LuoShuHost()
         }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        openTaskCenter = intent.getBooleanExtra(EXTRA_OPEN_TASK_CENTER, false)
     }
 }
