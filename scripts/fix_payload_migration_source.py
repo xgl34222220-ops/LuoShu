a06c0f954e02b9a2771324dfdbe43be7ff1f8562
xgl34222220-ops <xgl34222220@gmail.com>
2026-07-22 17:01:35 +0800

    chore: generate migration replacement line by line

--- scripts/fix_payload_migration_source.py
+++ scripts/fix_payload_migration_source.py
@@ -7,14 +7,15 @@ start_token = "state = state.replace(\n    '        \"$_module/config/app_instal
 end_token = "state = state.replace(\n    '''        cache/full-composite-v5"
 start = text.index(start_token)
 end = text.index(end_token, start)
-replacement = '''state = state.replace(
-    """        "$_module/config/app_install_manual" \\
-        "$_module/.font_switch.lock"""",
-    """        "$_module/config/app_install_manual" \\
-        "$_module/config/font-payload-rebuild-pending.conf" \\
-        "$_module/.font_switch.lock"""",
-    1,
+replacement = (
+    "state = state.replace(\n"
+    "    '''        \"$_module/config/app_install_manual\" \\\\\n"
+    "        \"$_module/.font_switch.lock\"''',\n"
+    "    '''        \"$_module/config/app_install_manual\" \\\\\n"
+    "        \"$_module/config/font-payload-rebuild-pending.conf\" \\\\\n"
+    "        \"$_module/.font_switch.lock\"''',\n"
+    "    1,\n"
+    ")\n"
 )
-'''
 path.write_text(text[:start] + replacement + text[end:], encoding="utf-8")
 print("payload migration quoting fixed")
