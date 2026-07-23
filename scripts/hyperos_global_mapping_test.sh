bf0fadd4b3533e20b0211619e0ef9700eddd5e46
xgl34222220-ops <xgl34222220@gmail.com>
2026-07-23 15:33:45 +0800

    test(hyperos): cover mi_ext and clock font slots

--- scripts/hyperos_global_mapping_test.sh
+++ scripts/hyperos_global_mapping_test.sh
@@ -10,10 +10,11 @@ USER_FONTS_DIR="$ROOT/user-fonts"
 LUOSHU_SYSTEM_FONTS_ROOT="$ROOT/real/system/fonts"
 LUOSHU_PRODUCT_FONTS_ROOT="$ROOT/real/product/fonts"
 LUOSHU_SYSTEM_EXT_FONTS_ROOT="$ROOT/real/system_ext/fonts"
-export MODULE_DIR MODDIR USER_FONTS_DIR LUOSHU_SYSTEM_FONTS_ROOT LUOSHU_PRODUCT_FONTS_ROOT LUOSHU_SYSTEM_EXT_FONTS_ROOT
+LUOSHU_MI_EXT_FONTS_ROOT="$ROOT/real/mi_ext/fonts"
+export MODULE_DIR MODDIR USER_FONTS_DIR LUOSHU_SYSTEM_FONTS_ROOT LUOSHU_PRODUCT_FONTS_ROOT LUOSHU_SYSTEM_EXT_FONTS_ROOT LUOSHU_MI_EXT_FONTS_ROOT
 
-mkdir -p "$MODULE_DIR/system/fonts" "$MODULE_DIR/product/fonts" "$MODULE_DIR/system_ext/fonts" \
-    "$USER_FONTS_DIR" "$LUOSHU_SYSTEM_FONTS_ROOT" "$LUOSHU_PRODUCT_FONTS_ROOT" "$LUOSHU_SYSTEM_EXT_FONTS_ROOT"
+mkdir -p "$MODULE_DIR/system/fonts" "$MODULE_DIR/product/fonts" "$MODULE_DIR/system_ext/fonts" "$MODULE_DIR/mi_ext/fonts" \
+    "$USER_FONTS_DIR" "$LUOSHU_SYSTEM_FONTS_ROOT" "$LUOSHU_PRODUCT_FONTS_ROOT" "$LUOSHU_SYSTEM_EXT_FONTS_ROOT" "$LUOSHU_MI_EXT_FONTS_ROOT"
 printf 'regular-source\n' > "$USER_FONTS_DIR/Demo-Regular.ttf"
 printf 'medium-source\n' > "$USER_FONTS_DIR/Demo-Medium.ttf"
 printf 'bold-source\n' > "$USER_FONTS_DIR/Demo-Bold.ttf"
@@ -26,6 +27,8 @@ printf 'stock-italic\n' > "$LUOSHU_SYSTEM_FONTS_ROOT/Roboto-Italic.ttf"
 printf 'stock-google-regular\n' > "$LUOSHU_PRODUCT_FONTS_ROOT/GoogleSansText-Regular.ttf"
 printf 'stock-google-medium\n' > "$LUOSHU_PRODUCT_FONTS_ROOT/GoogleSansText-Medium.ttf"
 printf 'stock-google-bold\n' > "$LUOSHU_PRODUCT_FONTS_ROOT/GoogleSansText-Bold.ttf"
+printf 'stock-clock\n' > "$LUOSHU_MI_EXT_FONTS_ROOT/MiClock.otf"
+printf 'stock-mitype\n' > "$LUOSHU_PRODUCT_FONTS_ROOT/MitypeClock.otf"
 printf 'stale-overlay\n' > "$MODULE_DIR/system/fonts/Roboto-Regular.ttf"
 
 _font_store_reset() {
@@ -67,6 +70,9 @@ test ! -e "$MODULE_DIR/system/fonts/Roboto-Italic.ttf"
 test "$(cat "$MODULE_DIR/product/fonts/GoogleSansText-Regular.ttf")" = 'regular-source'
 test "$(cat "$MODULE_DIR/product/fonts/GoogleSansText-Medium.ttf")" = 'medium-source'
 test "$(cat "$MODULE_DIR/product/fonts/GoogleSansText-Bold.ttf")" = 'bold-source'
+test "$(cat "$MODULE_DIR/mi_ext/fonts/MiClock.otf")" = 'regular-source'
+test "$(cat "$MODULE_DIR/product/fonts/MitypeClock.otf")" = 'regular-source'
+_luoshu_hyperos_root_pairs | grep -q "$LUOSHU_MI_EXT_FONTS_ROOT|$MODULE_DIR/mi_ext/fonts"
 printf 'HyperOS global mapping tests passed.\n'
 
 # Keep OEM partition regressions in the always-on source gate.
